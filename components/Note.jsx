feac05d36f461066ec45e9c4c52025ca3ac66a42
Manipi Harsha <74893267+Harshamanipi@users.noreply.github.com>
2021-06-06 09:51:27 +0530

    Add files via upload

---
+++ components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note () {
+  return (
+     <div>
+       <h1>Java script and react.js</h1>
+       <p>This was an amazing bootcamp taken by Shaurya singh Evrything from scratch including javascript and react.js</p>
+     </div>
+  );
+}
+
+export default Note;
